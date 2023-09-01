ba95da445c6de17143cd3e43a67054e8dfc6d669
Sher <sherdev@dr-tech.co>
2023-09-01 15:47:07 +0700

    Add Init GraphQL, Express Example

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # freelancer-express-graphql
 The simplest way to run a GraphQL API server is to use Express, a popular web application framework for Node.js. This example show you how to query freelancer profile with ID and Category from the data source.
+
+## Install
+
+```bash
+$ npm init
+```
+
+```bash
+$ npm i graphql express express-graphql -–save
+```
+
+## Change Type as Module in file package.json
+
+```bash
+$ "type": "module"
+```
+
+## Add key ```start``` in script  with value ```node server.js```
+
+```bash
+$ "start": "node server.js"
+```
+
+# Run Server
+
+```bash
+$ npm start
+
+```
+
+## Stay in touch
+- Author - [Sophat Chhay](https://sophat.bio)
+- Website - [https://expressjs.com](https://expressjs.com/)
---
+++ server.js
@@ -0,0 +1,59 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+import { freelancers } from "./graphql-data/data.js";
+
+// create GraphQL schema
+const schema = buildSchema(`
+    type Query {
+          freelancer(id: Int!): Freelancer
+          freelancers(category: String): [Freelancer]
+    }
+    type Freelancer {
+        id: Int
+        name: String
+        category: String
+        expertise: String
+        location: String
+        skills: [String]
+        profilePicture: String
+    }
+`);
+
+// Find specific freelancer with id
+const findFreelancer = (args) => {
+  for (const freelancer of freelancers) {
+      if (freelancer.id === args.id) {
+          return freelancer;
+      }
+  }
+}
+
+// Find freelancers with tech category
+const findFreelancersByCatId = (args) => {
+  if (args.category) {
+      return freelancers.filter((freelancer) => freelancer.category.toLowerCase() === args.category.toLowerCase());
+  } else {
+    return freelancers;
+  }
+}
+
+// Root resolver
+const graphqlData = {
+    freelancer: findFreelancer,
+    freelancers: findFreelancersByCatId,
+};
+
+// Create an express server and a GraphQL endpoint
+const app = express();
+const port = 4000;
+app.use("/graphql", graphqlHTTP({
+      schema: schema,
+      rootValue: graphqlData,
+      graphiql: true,
+    })
+);
+
+app.listen(port, () => {
+    console.log(`Express GraphQL Server is running a GraphQL server at http://localhost:${port}/graphql`)
+});
